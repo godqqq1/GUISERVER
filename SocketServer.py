083be16c7a801b1f11fd28d8135f15082df28e62
godqqq1 <godqqq1@nate.com>
2016-11-29 16:13:39 +0900

    Update SocketServer.py

--- SocketServer.py
+++ SocketServer.py
@@ -1,1 +1,73 @@
-# -*- coding: utf8 -*-# socket 과 select 모듈 임포트from socket import *from select import *import sysfrom time import ctime# 호스트, 포트와 버퍼 사이즈를 지정HOST = '127.0.0.1'PORT = 80BUFSIZE = 1024ADDR = (HOST, PORT)# 소켓 객체를 만들고..serverSocket = socket(AF_INET, SOCK_STREAM)# 서버 정보를 바인딩serverSocket.bind(ADDR)# 요청을 기다림(listen)serverSocket.listen(10)connection_list = [serverSocket]print('==============================================')print('채팅 서버를 시작합니다. %s 포트로 접속을 기다립니다.' % str(PORT))print('==============================================')# 무한 루프를 시작while connection_list:    try:        print('[INFO] 요청을 기다립니다...')        # select 로 요청을 받고, 10초마다 블럭킹을 해제하도록 함        read_socket, write_socket, error_socket = select(connection_list, [], [], 10)        for sock in read_socket:            # 새로운 접속            if sock == serverSocket:                clientSocket, addr_info = serverSocket.accept()                connection_list.append(clientSocket)                print('[INFO][%s] 클라이언트(%s)가 새롭게 연결 되었습니다.' % (ctime(), addr_info[0]))                # 클라이언트로 응답을 돌려줌                for socket_in_list in connection_list:                    if socket_in_list != serverSocket and socket_in_list != sock:                        try:                            socket_in_list.send('[%s] 새로운 방문자가 대화방에 들어왔습니다. 반가워요~ 짝짝짝!' % ctime())                        except Exception as e:                            socket_in_list.close()                            connection_list.remove(socket_in_list)            # 접속한 사용자(클라이언트)로부터 새로운 데이터 받음            else:                data = sock.recv(BUFSIZE)                if data:                    print('[INFO][%s] 클라이언트로부터 데이터를 전달 받았습니다.' % ctime())                    for socket_in_list in connection_list:                        if socket_in_list != serverSocket and socket_in_list != sock:                            try:                                socket_in_list.send('[%s] %s' % (ctime(), data))                                print('[INFO][%s] 클라이언트로 데이터를 전달합니다.' % ctime())                            except Exception as e:                                print(e.message)                                socket_in_list.close()                                connection_list.remove(socket_in_list)                                continue                else:                    connection_list.remove(sock)                    sock.close()                    print('[INFO][%s] 사용자와의 연결이 끊어졌습니다.' % ctime())    except KeyboardInterrupt:        # 부드럽게 종료하기        serverSocket.close()        sys.exit()
+from socket import *
+from select import *
+import sys
+from time 
+import ctime
+HOST = '127.0.0.1'
+PORT = 80
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(ADDR)
+serverSocket.listen(10)
+connection_list = [serverSocket]
+print('==============================================')
+print('Server Start. Port : %s ' % str(PORT))
+print('==============================================')
+while connection_list:    
+  try:        
+    print('[INFO] Wait...')
+        read_socket, write_socket, error_socket = select(connection_list, [], [], 10)
+        for sock ifrom socket import *
+from select import *
+import sys
+from time 
+import ctime
+HOST = '127.0.0.1'
+PORT = 80
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(ADDR)
+serverSocket.listen(10)
+connection_list = [serverSocket]
+print('==============================================')
+print('Server Start. Port : %s ' % str(PORT))
+print('==============================================')
+while connection_list:    
+  try:        
+    print('[INFO] Wait...')
+        read_socket, write_socket, error_socket = select(connection_list, [], [], 10)
+        for sock in read_socket:            
+          if sock == serverSocket:                
+            clientSocket, addr_info = serverSocket.accept()                
+            connection_list.append(clientSocket)                
+            print('[INFO][%s] Client (%s) is Connected.' % (ctime(), addr_info[0]))
+            for socket_in_list in connection_list:                    
+              if socket_in_list != serverSocket and socket_in_list != sock:                        
+                try:                            
+                  socket_in_list.send('[%s] New Client come. Hello!' % ctime())                        
+                except Exception as e:                            
+                  socket_in_list.close()                            
+                  connection_list.remove(socket_in_list)            
+         else:                
+          data = sock.recv(BUFSIZE)                
+          if data:                    
+            print('[INFO][%s] Client send Message' % ctime())                    
+            for socket_in_list in connection_list:                        
+              if socket_in_list != serverSocket and socket_in_list != sock:                            
+                try:                                
+                  socket_in_list.send('[%s] %s' % (ctime(), data))                                
+                  print('[INFO][%s] Send Message.' % ctime())                            
+                except Exception as e:                                
+                  print(e.message)                                
+                  socket_in_list.close()                                
+                  connection_list.remove(socket_in_list)                                
+                  continue                
+          else:                    
+            connection_list.remove(sock)                    
+            sock.close()                    
+            print('[INFO][%s] Black out' % ctime())    
+  except KeyboardInterrupt:        
+    serverSocket.close()        
+     sys.exit()
