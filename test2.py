bb5ef27fafda1501b2de833cfcb5802a581417d1
godqqq1 <godqqq1@nate.com>
2016-11-30 09:09:40 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 50007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(1)
+while 1:
+    conn, addr = s.accept()
+    print ('Connected by', addr)
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
