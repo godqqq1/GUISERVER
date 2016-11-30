c52e0165f627ae1e0e023cdc9b373fc09d6cb67c
godqqq1 <godqqq1@nate.com>
2016-11-30 16:40:14 +0900

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Frame, BOTH
+import tkinter
+from PIL import Image, ImageTk
+root = tkinter()
+image2 =Image.open('CoolingTower.png')
+background_image=root.PhotoImage(image2)
+background_label = root.Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+print(screen_width,screen_height)
+root.attributes("-fullscreen", True)
+root.mainloop()
