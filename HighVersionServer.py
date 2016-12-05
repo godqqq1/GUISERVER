65f08d9ae64c9480e25f7c6d5b382c8604d63287
godqqq1 <godqqq1@nate.com>
2016-12-05 07:29:24 +0000

    1

--- HighVersionServer.py
+++ HighVersionServer.py
@@ -2,12 +2,14 @@ from select import *
 import sys
 import socket
 
-HOST = '192.168.8.100'
+HOST = '192.168.8.200'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST,PORT))
 s.listen(1)
+timer = 10
 while 1:
+	print(timer)
    	conn, addr = s.accept()
 	data = conn.recv(1024)
 	if data:
@@ -15,7 +17,11 @@ while 1:
 	else:
 		conn.close()
 	try:
-		conn.sendall('abc\r')
+		if(timer == 0) :
+			conn.sendall('kill\r')
+		else :
+			timer = timer - 1
+			conn.sendall('abc\r')
 	except Exception as e:
 		conn.close()
 		print("Lost Conn")
--- test3.py
+++ test3.py
@@ -1,11 +1,36 @@
+from PIL import ImageTk
+import PIL.Image
 import Tkinter as tk
-from PIL import Image, ImageTk
-
 root = tk.Tk()
-image2 =Image.open('CoolingTower.png')
-background_image=root.PhotoImage(image2)
-background_label = root.Label(root, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+photo = ImageTk.PhotoImage(PIL.Image.open('CoolingTower.png'))
+labimg = tk.Label(root,image=photo)
+labimg.image = photo
+labimg.place(x=-50,y=-20)
+rpm1 = tk.Label(root, text = 'RPM:',font = ("Courier",20))
+rpm1.place(x=50,y=20)
+rpm2 = tk.Label(root, text = '123',font = ("Courier",20))
+rpm2.place(x=120,y=20)
+pwm1 = tk.Label(root, text = 'Watt:',font = ("Courier",20))
+pwm1.place(x=500,y=360)
+pwm2 = tk.Label(root, text = '123',font = ("Courier",20))
+pwm2.place(x=580,y=360)
+iwt1 = tk.Label(root, text = 'IWT:',font = ("Courier",20))
+iwt1.place(x=600,y=60)
+iwt2 = tk.Label(root, text = '123',font = ("Courier",20))
+iwt2.place(x=680,y=60)
+owt1 = tk.Label(root, text = 'OWT:',font = ("Courier",20))
+owt1.place(x=70,y=360)
+owt2 = tk.Label(root, text = '123',font = ("Courier",20))
+owt2.place(x=130,y=360)
+iwt1 = tk.Label(root, text = 'AT:',font = ("Courier",20))
+iwt1.place(x=600,y=220)
+iwt2 = tk.Label(root, text = '123',font = ("Courier",20))
+iwt2.place(x=680,y=220)
+owt1 = tk.Label(root, text = 'AH:',font = ("Courier",20))
+owt1.place(x=600,y=250)
+owt2 = tk.Label(root, text = '123',font = ("Courier",20))
+owt2.place(x=680,y=250)
+
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 print(screen_width,screen_height)
